6d93f67be28a34ff842d46e12f2f08af48895eec
Dan <danilobomfim.si@gmail.com>
2022-08-05 23:02:28 -0300

    Refactor, modificação no nome de algumas pastas e criado novos arquivos.

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,10 +1,14 @@
-import Banner from './components/banner/Banner.js'
+import Banner from './components/Banner/Banner.js'
+import CampoTexto from './components/CampoTexto/CampoTexto.js';
 
 function App() {
   return (
     
     <div className="App">
       <Banner/>
+      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>  
     </div>
   );
 }
