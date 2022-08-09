52eeca8d6adcf04697538cabb45005d94f0a933d
Dan <danilobomfim.si@gmail.com>
2022-08-08 21:11:21 -0300

    Feat capturando texto ao ser digitado

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,23 @@
 import "./CampoTexto.css";
 
+let valor = "";
+
+/* Em desenvolvimento. */
+const novoValor = (evento) => {
+  valor = evento.target.value;
+  console.log(valor);
+};
+
 function CampoTexto(props) {
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input required={props.required} type="text" placeholder={props.placeholder}/>
+      <input
+        onChange={novoValor}
+        required={props.required}
+        type="text"
+        placeholder={props.placeholder}
+      />
     </div>
   );
 }
