b365c60afa68be6465f62c8cc295c9f0b01ccf30
Dan <danilobomfim.si@gmail.com>
2022-08-11 22:41:24 -0300

    Criando o componente de equipes

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,22 +1,40 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner/Banner.js";
 import Formulario from "./componentes/Form/Formulario.js";
+import Team from "./componentes/Team";
+
+/* Times
+
+"Programação",
+  "Front-End",
+  "Data Science",
+  "Devops",
+  "Ux e Design",
+  "Mobile",
+  "Inovação e Gestão",
+
+*/
 
 function App() {
-  const [cards, setCard] = useState([])
+  const [cards, setCard] = useState([]);
 
-
-
-  const cadastro = (novoCard) =>{
-   /* Spread */
-   setCard([...cards, novoCard])
-   console.log(cards)
-  }
+  const cadastro = (novoCard) => {
+    /* Spread */
+    setCard([...cards, novoCard]);
+    console.log(cards);
+  };
 
   return (
     <div className="App">
       <Banner />
-      <Formulario novoCard={novoCard => cadastro(novoCard)}/>
+      <Formulario novoCard={(novoCard) => cadastro(novoCard)} />
+      <Team name="Programação" />
+      <Team name="Front-End" />
+      <Team name="Data Science" />
+      <Team name="Devops" />
+      <Team name="Ux e Design" />
+      <Team name="Mobile" />
+      <Team name="Inovação e Gestão" />
     </div>
   );
 }
