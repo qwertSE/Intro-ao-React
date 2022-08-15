ff4c1f34a36491163446f153f1dd4f3f7e636645
Dan <danilobomfim.si@gmail.com>
2022-08-14 23:04:09 -0300

    refactor funcoes

--- organo/src/App.js
+++ organo/src/App.js
@@ -19,38 +19,38 @@ function App() {
     {
       nome: "Programação",
       corPrimaria: "#57C278",
-      corSecundaria: "#D9F7E9"
+      corSecundaria: "#D9F7E9",
     },
     {
       nome: "Front-End",
       corPrimaria: "#82CFFA",
-      corSecundaria: "#E8F8FF"
+      corSecundaria: "#E8F8FF",
     },
     {
       nome: "Data Science",
       corPrimaria: "#A6D157",
-      corSecundaria: "#F0F8E2"
+      corSecundaria: "#F0F8E2",
     },
     {
       nome: "Devops",
       corPrimaria: "#E06B69",
-      corSecundaria: "#FDE7E8"
+      corSecundaria: "#FDE7E8",
     },
     {
       nome: "Ux e Design",
       corPrimaria: "#DB6EBF",
-      corSecundaria: "#FAE9F5"
+      corSecundaria: "#FAE9F5",
     },
     {
       nome: "Mobile",
       corPrimaria: "#FFBA05",
-      corSecundaria: "#FFF5D9"
+      corSecundaria: "#FFF5D9",
     },
     {
       nome: "Inovação e Gestão",
       corPrimaria: "#FF8A29",
-      corSecundaria: "#FFEEDF"
-    }
+      corSecundaria: "#FFEEDF",
+    },
   ];
 
   const [cards, setCard] = useState([]);
@@ -65,7 +65,17 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario novoCard={(novoCard) => cadastro(novoCard)} />
-      {times.map(time => (<Team key={time.nome} name={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>))}
+      {times.map((time) => (
+        <Team
+          key={time.nome}
+          name={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+          colaboradores={cards.filter(
+            (colaborador) => colaborador.time === time.nome
+          )}
+        />
+      ))}
     </div>
   );
 }
--- organo/src/componentes/Team/index.js
+++ organo/src/componentes/Team/index.js
@@ -1,11 +1,22 @@
+import Colaborador from "../Colaborador";
 import "./team.css";
 
 const Team = (props) => {
-    return (
-        <section className="team" style={{backgroundColor: props.corSecundaria}}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.name}</h3>
-        </section>
-    )
+  return (
+    <section className="team" style={{ backgroundColor: props.corSecundaria }}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.name}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            img={colaborador.imagem}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Team;
