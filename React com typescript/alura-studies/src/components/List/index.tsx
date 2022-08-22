ea65c4136d4d54da5a2d460c79dc5fb743cad6ee
Dan <danilobomfim.si@gmail.com>
2022-08-21 22:32:52 -0300

    refactor: adicionado useState

--- React com typescript/alura-studies/src/components/List/index.tsx
+++ React com typescript/alura-studies/src/components/List/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Item from "./Item";
 import style from "./List.module.scss";
 
 const List = () => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: "React",
       time: "02:00:00",
@@ -15,17 +16,21 @@ const List = () => {
       task: "MongoDB",
       time: "01:30:00",
     },
-  ];
+  ]);
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { task: "Java", time: "00:30:00" }]);
+        }}
+      >
+        {" "}
+        Estudos do dia{" "}
+      </h2>
 
       <ul>
         {tasks.map((item, index) => (
-          <Item
-            key={index}
-            {...item}
-          />
+          <Item key={index} {...item} />
         ))}
       </ul>
     </aside>
