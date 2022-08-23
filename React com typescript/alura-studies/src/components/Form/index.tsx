87eb57f8b7cc8983ad8ec4867003af3dd22066d9
Dan <danilobomfim.si@gmail.com>
2022-08-22 22:03:13 -0300

    Cancelando efeito padrão do submit

--- React com typescript/alura-studies/src/components/Form/index.tsx
+++ React com typescript/alura-studies/src/components/Form/index.tsx
@@ -1,36 +1,40 @@
+import React from "react";
 import Button from "../Button";
 import style from "./Form.module.scss";
 
+function adicionarTarefa(event: React.FormEvent) {
+  event.preventDefault();
+  console.log("Teste");
+}
+
 const Form = () => {
-    return (
-      <form className={style.novaTarefa}>
-        <div className={style.inputContainer}>
-          <label htmlFor="tarefa">Adicione um novo estudo</label>
-          <input
-            type="text"
-            name="tarefa"
-            id="tarefa"
-            placeholder="O que você quer estudar"
-            required
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <label htmlFor="tempo">Tempo</label>
-          <input
-            type="time"
-            step="1"
-            name="tempo"
-            id="tempo"
-            min="00:00:00"
-            max="01:30:00"
-            required
-          />
-        </div>
-        <Button >
-          Adicionar
-         </Button>
-      </form>
-    );
-  }
+  return (
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+      <div className={style.inputContainer}>
+        <label htmlFor="tarefa">Adicione um novo estudo</label>
+        <input
+          type="text"
+          name="tarefa"
+          id="tarefa"
+          placeholder="O que você quer estudar"
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          id="tempo"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+      </div>
+      <Button>Adicionar</Button>
+    </form>
+  );
+};
 
 export default Form;
